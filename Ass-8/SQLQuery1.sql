b245e1305528250d2709ca2e609d3438b7609271
NguyenTruong150197 <truong12121996@gmail.com>
2022-10-14 18:47:34 +0700

    ass2

--- Ass-8/SQLQuery1.sql
+++ Ass-8/SQLQuery1.sql
@@ -1,30 +1,30 @@
-create table Subjects(
+﻿create table Subjects(
 	Code int primary key,
-	Name varchar(255)
+	Name varchar(255) not null unique
 );
 
 create table Classes(
 	Code int primary key,
-	Name varchar(255),
+	Name varchar(255) not null unique,
 	Room varchar(10)
 );
 
 create table Students(
 	Code int primary key,
-	Name varchar(255),
-	Birthday date,
+	Name varchar(255) not null,
+	Birthday date not null check(Birthday < getdate()),
 	ClassCode int foreign key references Classes(Code)
 );
 
-create table StudentSubjects(
-	StudentCode int foreign key references Students(Code),
-	SubjectCode int foreign key references Subjects(Code)
+create table Results(
+	ID int primary key identity(1, 1),
+	Mark int not null check(Mark >= 0 and Mark <= 10) default 0,
+	Result varchar(255) not null check(Result in (N'Yếu', N'Trung Bình', N'Khá', N'Giỏi', N'Xuất Xắc')),
+	StudentCode int not null foreign key references Students(Code),
+	SubjectCode int not null foreign key references Subjects(Code)
 );
 
-create table Results(
-	ID int primary key,
-	Mark int,
-	Result varchar(255),
-	StudentCode int foreign key references Students(Code),
-	SubjectCode int foreign key references Subjects(Code)
+create table StudentSubjects(
+	StudentCode int not null foreign key references Students(Code),
+	SubjectCode int not null foreign key references Subjects(Code)
 );
