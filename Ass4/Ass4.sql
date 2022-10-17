0fa801d76eba1ebba77979a2d83e2820d5f7824c
NguyenTruong150197 <truong12121996@gmail.com>
2022-10-17 17:49:31 +0700

    ass4

---
+++ Ass4/Ass4.sql
@@ -0,0 +1,35 @@
+create table NHACUNGCAP(
+	MaNhaCC varchar(6) primary key,
+	TenNhaCC nvarchar(255) not null unique,
+	DiaChi varchar(20) not null,
+	SoDT varchar(15) not null check(SoDT like N'0%'),
+	MaSoThue int not null unique
+);
+
+create table LOAIDICHVU(
+	MaLoaiDV varchar(4) primary key,
+	TenLoaiDV nvarchar(255) not null unique
+);
+
+create table MUCPHI(
+	MaMP varchar(4) primary key,
+	DonGia decimal(10,0) not null,
+	Mota nvarchar(50)
+);
+
+create table DONGXE(
+	DongXe varchar(15) primary key,
+	HangXe varchar(10) not null check(HangXe in ('Toyota', 'Ford', 'KIA', 'Huyndai')),
+	SoChoNgoi int not null
+);
+
+create table DANGKYCUNGCAP(
+	MaDKCC varchar(5) primary key,
+	MaNCC varchar(6) not null foreign key references NHACUNGCAP(MaNCC),
+	MaLoaiDV varchar(4) not null foreign key references LOAIDICHVU(MaLoaiDV),
+	DongXe varchar(15) not null foreign key references DONGXE(DongXe),
+	MaMP varchar(4) not null foreign key references MUCPHI(MaMP),
+	NgayBatDauCungCap date check(NgayBatDauCungCap <= Getdate()),
+	NgayKetThucCungCap date check(NgayKetThucCungCap > Getdate()),
+	SoLuongXeDangKy int
+);
---
+++ Buoi4/SQLQuery2.sql
@@ -0,0 +1,38 @@
+﻿insert into Customers(Name, Address, Tel)
+values(N'Nguyễn Văn An', N'111 Nguyễn Trãi, Thanh Xuân', '0987654321');
+
+select * from Customers;
+
+insert into Products(Name, Description, Unit, Price)
+values(N'Máy Tính T450', N'Máy nhập mới', N'Chiếc', 1000);
+
+insert into Products(Name, Description, Unit, Price)
+values(N'Điện Thoại Nokia5670', N'Điện thoại đang hot', N'Chiếc', 200);
+
+insert into Products(Name, Description, Unit, Price)
+values(N'Máy In Samsung450', N'Máy in đang ế', N'Chiếc', 100);
+
+/*insert into Products(Name, Description, Unit, Price)
+  values(N'Máy Tính T450', N'Máy nhập mới', N'Chiếc', 1000),
+  (N'Điện Thoại Nokia5670', N'Điện thoại đang hot', N'Chiếc', 200),
+  (N'Máy In Samsung450', N'Máy in đang ế', N'Chiếc', 100);
+*/
+
+select * from Products;
+
+insert into  Orders(OrderDate, GrandTotal, CustomerID)
+values('2022-09-10', 1500, 1);
+
+select * from Orders;
+
+insert into OrderItems(Qty, Total, ProductID, OrderID)
+values(1, 1000, 1, 1),
+(2, 400, 2, 1),
+(1, 100, 3, 1);
+
+select * from OrderItems;
+
+update Customers set Address = N'Số 8 Tôn Thất Thuyết, Nam Từ Liêm'
+where ID = 1;
+
+delete from Customers;
